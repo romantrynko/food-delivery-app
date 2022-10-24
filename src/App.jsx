9ae6eac2a9336a0549a97a6d38370fd0c75ed70e
Roman Trynko <trynkoroman25@gmail.com>
2022-10-24 11:01:34 +0300

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="w-screen h-screen flex items-center justify-center text-blue-600">
+      App
+    </div>
+  );
+};
 
-export default App
+export default App;
